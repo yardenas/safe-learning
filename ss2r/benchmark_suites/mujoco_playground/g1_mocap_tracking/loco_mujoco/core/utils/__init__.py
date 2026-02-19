c3631ac7a2588130e933533c485484a910de7fac
Yarden As <yardas@amazon.de>
2026-02-19 17:20:11 +0100

    Fix

--- ss2r/benchmark_suites/mujoco_playground/g1_mocap_tracking/loco_mujoco/core/utils/__init__.py
+++ ss2r/benchmark_suites/mujoco_playground/g1_mocap_tracking/loco_mujoco/core/utils/__init__.py
@@ -1,12 +1,3 @@
-# Keep reward classes re-exported, but import them after core utility symbols so
-# reward modules can safely import `core.utils` during package initialization.
-from ..reward.default import (
-    LocomotionReward,
-    NoReward,
-    TargetVelocityGoalReward,
-    TargetXVelocityReward,
-)
-from ..reward.trajectory_based import MimicReward, TargetVelocityTrajReward
 from .backend import *
 from .decorators import info_property
 from .env import Box, MDPInfo
